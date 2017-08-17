c854b89be697dbf5bf923910b597a08ea0fad509
vamsi12a2 <vamsi12a2@gmail.com>
2017-08-17 06:15:44 -0400

    modified:   controllers/index.js 	modified:   server.js 	new file:   views/index.html

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,13 @@
 var express = require('express')
 var router = express.Router()
 
-router.use('/user',require('./user'))
+router.use('/user', require('./user'))
+router.use(express.static(__dirname + '/../views'))
+
+router.get("/", function(req, res, err) {
+  res.render("index.html")
+});
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,15 +4,10 @@ var http=require('http');
 var express = require('express')
 var config = require('./config')
 var app=express()
-
-
+app.use(require('./controllers'))
 var server = app.listen(config.port,function() {
                  
   console.log("Server is up and listening...")
 });
 
 
-app.use("/",function(req,res){
-  res.write("hello");
-  res.end();
-});
