242b0b86fcdde6009ba860e2f322246da9bc42c5
vamsi12a2 <vamsi12a2@gmail.com>
2017-08-17 03:26:57 -0400

    added some routes

--- server.js
+++ server.js
@@ -2,17 +2,21 @@
 
 var http=require('http');
 var express = require('express')
+var body_par = require('body-parser')
+var cookie_par = require('cookie-parser')
 var config = require('./config')
 var app=express()
-
-
-var server = app.listen(config.port,function() {
+app.use(body_par.json())
+app.use(cookie_par())
+app.use(require('./controllers'))
+var server = app.listen(config.port,function() 
+{
                  
   console.log("Server is up and listening...")
 });
 
 
-app.use("/",function(req,res){
+/*app.use("/",function(req,res){
   res.write("hello");
   res.end();
-});
+});*/
